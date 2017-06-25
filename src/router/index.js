b0eba21aa40262d64eb50777ff10c03bdec0288f
zhangqijie <270217737@qq.com>
2017-06-25 21:17:09 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+        routes: [{
+            path: '/',
+            name: 'Hello',
+            component: Hello
+        }, {
+            path: '/home',
+            name: 'Home',
+            component: Home
+        }]
+    })
+    // 注册
+Vue.component('my-component', {
+        template: '<div>A custom component!</div>'
+    })
+    // 创建根实例
+new Vue({
+    el: '#example'
 })
