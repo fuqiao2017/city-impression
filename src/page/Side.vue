e0f0c3c87059da27bd14714e0ff7d15fce2de006
fuqiao2017 <270217737@qq.com>
2017-06-30 19:55:08 +0800

    11

---
+++ src/page/Side.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <span><i>K</i></span><span>登录/注册</span>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  data () {
+      return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
+
